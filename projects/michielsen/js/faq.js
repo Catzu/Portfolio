34a74e450dbb6a1c433f00fdf6a299b8823ed16f
Ngoc <nt7nguyen@gmail.com>
2026-04-01 00:14:16 +0200

    reworked changes

--- projects/michielsen/js/faq.js
+++
@@ -1,6 +0,0 @@
-function toggleFaq(el) {
-  const item = el.closest('.faq-item');
-  const allItems = document.querySelectorAll('.faq-item.open');
-  allItems.forEach(i => { if (i !== item) i.classList.remove('open'); });
-  item.classList.toggle('open');
-}
--- projects/michielsen/js/preventDefault.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('click', e => {
-  const a = e.target.closest('a[href="#"]');
-  if (a) e.preventDefault();
-});
